ba3f6587da4ae16678b8c1f58d47e833af50ef8f
Michalina Oleksy <layika@protonmail.com>
2017-04-05 20:10:28 +0200

    Fixed amount of spaces before comments

--- insomnihack(2017)/capscii/solve.py
+++ insomnihack(2017)/capscii/solve.py
@@ -18,27 +18,27 @@ r = req.get(url)
 
 for i in range(50):
     s = soup(r.text)
-    chunks = s.findAll(text=True)[6:11]                                           # takes rows of digits and insters them into a list
+    chunks = s.findAll(text=True)[6:11]                  # takes rows of digits and insters them into a list
 
-    height = len(chunks)                                                          # height of every letter is the amount of chunks
-    for row in range(height):
-        chunks[row] = [chunks[row][i*5:i*5+3] for i in range(len(chunks[row])/5)] # 5 = 3 for letter, 2 for space between
+    height = len(chunks)                                 #height of every letter is the amount of chunks
+    for row in range(height):                            # 5 = 3 for letter, 2 for space between
+        chunks[row] = [chunks[row][i*5:i*5+3] for i in range(len(chunks[row])/5)]
 
     for l_chunk in range(len(chunks[0])):
-        if chunks[0][l_chunk] == '   ':                                           # we need to find the place where the sign is as numbers have different length
-            sign_idx = l_chunk                                                    # when we meet te sign the first row is empty so this is how we can recognize it
+        if chunks[0][l_chunk] == '   ':                  # we need to find the place where the sign is as numbers have different length
+            sign_idx = l_chunk                           # when we meet te sign the first row is empty so this is how we can recognize it
 
-    chunks_a = ['*']*height                                                       # in order to make it easier to recognize the digits we convert digits to '*'
-    for row in range(height):                                                     # as every time the digits are random
+    chunks_a = ['*']*height                              # in order to make it easier to recognize the digits we convert digits to '*'
+    for row in range(height):                            # as every time the digits are random
         chunks_a[row] = asteriskize(chunks[row])
 
-    digits = [[] for i in range(len(chunks_a[0]))]                                # now we divide chunks_a into digits:
+    digits = [[] for i in range(len(chunks_a[0]))]       # now we divide chunks_a into digits:
 
-    for row in range(height):                                                     # in order to do this we need to take from every row a part of the digit
+    for row in range(height):                            # in order to do this we need to take from every row a part of the digit
         for chunk in range(len(chunks_a[0])):
             digits[chunk].append(chunks_a[row][chunk])
 
-    zero = ['***', '* *', '* *', '* *', '***']                                    # patterns for digits and signs respectively
+    zero = ['***', '* *', '* *', '* *', '***']           # patterns for digits and signs respectively
     one = ['** ', ' * ', ' * ', ' * ', '***']
     two = ['***', '  *', '***', '*  ', '***']
     three = ['***', '  *', '***', '  *', '***']
@@ -59,7 +59,7 @@ for i in range(50):
 
     left = ''
     right = ''
-    for letter in digits[:sign_idx]:                                              # here we convert asteriskized digits into digits (first as strings)
+    for letter in digits[:sign_idx]:                    # here we convert asteriskized digits into digits (first as strings)
         if letter in chars:
             left += str(chars.index(letter))
 
